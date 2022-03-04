4322860a80f31b7fd5d0972356a01ecb330f1335
super-rogatory <chukwudi10@gmail.com>
2022-03-03 19:05:31 -0800

    can test locally using os and sys methods

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -1,11 +1,33 @@
+# For testing locally!
+# Making the directory in aima-python available to reference
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath("/home/super-rogatory/Desktop/Spring_2022/CPSC481/aima-python")
+)
+# For testing locally!
+
 from search import *
 
 # YOUR CODE GOES HERE
-# class WolfGoatCabbage(Problem):
+class WolfGoatCabbage(Problem):
+    def __init__(self, initial={"F", "W", "G", "C"}, goal=set()):
+        # Similar to EightPuzzle, we can define the initial and goal states for our problem.
+        super().__init__(initial, goal)
+
+    def goal_test(self, state):
+        return state == self.goal
+
+    def actions(self, state):
+        if state == {"F", "W", "G", "C"}:
+            return [{"F", "G"}]
+
 
 if __name__ == "__main__":
-    # wgc = WolfGoatCabbage()
-    solution = depth_first_graph_search(wgc).solution()
-    print(solution)
-    solution = breadth_first_graph_search(wgc).solution()
-    print(solution)
+    wgc = WolfGoatCabbage()
+    # solution = depth_first_graph_search(wgc).solution()
+    # print(solution)
+    # solution = breadth_first_graph_search(wgc).solution()
+    # print(solution)
+    print(wgc.actions())
