deda03b8448a884d26ee70be33fd1c344a5a0266
super-rogatory <chukwudi10@gmail.com>
2022-03-06 13:53:10 -0800

    feature: working depth and breadth first solutions

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -1,6 +1,4 @@
 # Chukwudi Ikem
-# TODO: Sititng at 15/25. So far everything has been brute force | take break | continue later tonight.
-
 # For testing locally!
 # Making the directory in aima-python available to reference
 import sys
@@ -18,44 +16,6 @@ class WolfGoatCabbage(Problem):
     def __init__(self, initial=frozenset({"F", "W", "G", "C"}), goal=frozenset()):
         # Similar to EightPuzzle, we can define the initial and goal states for our problem.
         super().__init__(initial, goal)
-
-    def goal_test(self, state):
-        return state == self.goal
-
-    def result(self, state, action):
-        new_result = set(state)
-        # TODO: fix result function.
-        # possible_actions = [{"F"}, {"F", "C"}, {"F", "G"}, {"F", "W"}]
-        if action == "F":
-            if "F" in new_result:
-                new_result.remove("F")
-            else:
-                new_result.add("F")
-
-        elif action == {"F", "C"}:
-            if "F" in new_result:
-                new_result.remove("F")
-                new_result.remove("G")
-            else:
-                new_result.add("F")
-                new_result.add("C")
-
-        elif action == {"F", "G"}:
-            if "F" in new_result:
-                new_result.remove("F")
-                new_result.remove("G")
-            else:
-                new_result.add("F")
-                new_result.add("G")
-
-        elif action == {"F", "W"}:
-            if "F" in new_result:
-                new_result.remove("F")
-                new_result.remove("W")
-            new_result.add("F")
-            new_result.add("W")
-
-        return frozenset(new_result)  # unhashable type error if result is not frozen
 
     def actions(self, state):
         # mirroring possible_actions list from eight_sliding_puzzle. acknowledge bidrectional potential.
@@ -79,10 +39,47 @@ class WolfGoatCabbage(Problem):
 
         return possible_actions
 
+    def result(self, state, action):
+        new_result = set(state)
+        # possible_actions = [{"F"}, {"F", "C"}, {"F", "G"}, {"F", "W"}]
+        if action == {"F"}:
+            if "F" in new_result:
+                new_result.remove("F")
+            else:
+                new_result.add("F")
+
+        elif action == {"F", "C"}:
+            if "F" in new_result:
+                new_result.remove("F")
+                new_result.remove("C")
+            else:
+                new_result.add("F")
+                new_result.add("C")
+
+        elif action == {"F", "G"}:
+            if "F" in new_result:
+                new_result.remove("F")
+                new_result.remove("G")
+            else:
+                new_result.add("F")
+                new_result.add("G")
+
+        elif action == {"F", "W"}:
+            if "F" in new_result:
+                new_result.remove("F")
+                new_result.remove("W")
+            else:
+                new_result.add("F")
+                new_result.add("W")
+
+        return frozenset(new_result)  # unhashable type error if result is not frozen
+
+    def goal_test(self, state):
+        return state == self.goal
+
 
 if __name__ == "__main__":
     wgc = WolfGoatCabbage()
-    print(wgc.result({"W"}, {"F", "C"}))
     solution = depth_first_graph_search(wgc).solution()
     print(solution)
     solution = breadth_first_graph_search(wgc).solution()
